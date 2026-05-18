2e3de8d1bcd920744d1b1ed2ae5811e446198a79
Gailcapulong <gailcapulong77@gmail.com>
2026-05-18 22:06:15 +0800

    Add server setup and package.json for initial configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,28 @@
-//const express = require('express');
-
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
 
+
+
 const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth")
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-app.listen(3000, () => console.log ("Server running on port 3000 1"));
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
