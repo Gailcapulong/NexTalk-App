0f46bd0de15225bc354bdcb8e88efbeae09f9d92
Gailcapulong <gailcapulong77@gmail.com>
2026-05-22 01:14:03 +0800

    Add Cloudinary integration for profile picture uploads and implement profile update functionality

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -10,18 +10,7 @@ export const ENV = {
   RESEND_API_KEY: process.env.RESEND_API_KEY,
   EMAIL_FROM: process.env.EMAIL_FROM,
   EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
-
-//PORT=3000
-//MONGO_URI=mongodb+srv://gailcapulong77_db_user:l3D6LxXElKFnGVyE@cluster0.torfxuh.mongodb.net/nextalk_db?appName=Cluster0
-
-//NODE_ENV=development
-
-//JWT_SECRET=myjstsecret
-
-//RESEND_API_KEY=re_i6by5XX8_FEEsfkGsYzTuDRaJbvTDVACt
-
-//EMAIL_FROM="onboarding@resend.dev"
-//EMAIL_FROM_NAME="Jennifer Cuni"
-
-//CLIENT_URL=http://localhost:5173
