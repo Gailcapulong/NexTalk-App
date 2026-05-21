33e7cec4fc9cf6a08bea5846b8e13cb6694d9e1c
Gailcapulong <gailcapulong77@gmail.com>
2026-05-22 00:08:27 +0800

    Implement login and logout functionality in auth controller and update routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,3 +68,34 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ messsage: "Invalid Credentials" });
+    // never tell  the client which one is incorrent: password or email
+
+    const isPaswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPaswordCorrect)
+      return res.status(400).json({ messsage: "Invalid Credentials" });
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in login controller:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = async (_req, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out succesfully " });
+};
