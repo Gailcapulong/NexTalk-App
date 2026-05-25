3bd5a22d7836e46e8d5af23a468758a4702c1048
Gailcapulong <gailcapulong77@gmail.com>
2026-05-26 00:43:49 +0800

    feat: implement chat and contact lists with loading states and no chat found component

--- frontend/src/components/ActiveTabSwitch.jsx
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useChatStore } from "../store/useChatStore";
 
 function ActiveTabSwitch() {
-  return <div>ActiveTabSwitch</div>;
-}
+  const { activeTab, setActiveTab } = useChatStore();
 
+  return (
+    <div className="tabs tabs-boxed bg-transparent p-2 m-2">
+      <button
+        onClick={() => setActiveTab("chats")}
+        className={`tab ${
+          activeTab === "chats"
+            ? "bg-blue-500/20 text-yellow-400"
+            : "text-slate-500"
+        }`}
+      >
+        Chats
+      </button>
+
+      <button
+        onClick={() => setActiveTab("contacts")}
+        className={`tab ${
+          activeTab === "contacts"
+            ? "bg-red-500/20 text-yellow-400"
+            : "text-slate-500"
+        }`}
+      >
+        Contacts
+      </button>
+    </div>
+  );
+}
 export default ActiveTabSwitch;
